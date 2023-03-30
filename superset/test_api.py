0d8147b9fd9d6d90ccada62a322fbc06cafeb703
Mainer-g00t <59738113+Mainer-g00t@users.noreply.github.com>
2023-03-30 22:36:36 +0200

    test

---
+++ superset/test_api.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from supersetapiclient.client import SupersetClient
+
+client = SupersetClient(
+    host="http://localhost:8080",
+    username="admin",
+    password="admin",
+)
